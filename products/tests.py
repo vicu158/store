08094480740c2043a4ff9235125108a3810c8579
vicu158 <v_evlashev@mail.ru>
2024-07-05 14:07:49 +0300

    vid 9.5

---
+++ products/tests.py
@@ -0,0 +1,53 @@
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Product, ProductCategory
+
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['categories.json', 'goods.json']
+
+    def setUp(self):
+        self.products = Product.objects.all()
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self._common_tests(response)
+
+    def test_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_id': category.id})
+        response = self.client.get(path)
+
+        self._common_tests(response)
+        # self.assertEqual(
+        #     list(response.context_data['object_list']),
+        #     list(self.products.filter(category_id=category.id))
+        # )
+
+        self.assertQuerysetEqual(response.context_data.get('object_list'), Product.objects.all()[:3], ordered=False)
+
+        product_list = []
+        for page in response.context_data['paginator']:
+            product_list.extend(list(page))
+        list(response.context_data['paginator'].page(1).object_list)
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
