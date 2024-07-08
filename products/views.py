adcdbe043da1e454b55124262adbde4a4c436776
vicu158 <v_evlashev@mail.ru>
2024-07-08 18:51:33 +0300

    b10.5.3 починил пагинацию и фильтрацию по категориям

--- products/views.py
+++ products/views.py
@@ -29,6 +29,7 @@ class ProductsListView(TitleMixin, ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductsListView, self).get_context_data()
         context['categories'] = ProductCategory.objects.all()
+        context['category_id'] = self.kwargs.get('category_id')
         return context
 
 
